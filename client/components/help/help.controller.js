b753549ebcdd7f1effdcd04dd69e6a06fbaa703d
alan <alancao101@gmail.com>
2016-07-18 18:24:22 -0400

    changed highchart's heatmap

--- client/components/help/help.controller.js
+++ client/components/help/help.controller.js
@@ -1,15 +1,3 @@
-// function convertMonthNameToNumber(monthName) {
-//     var myDate = new Date(monthName + " 1, 2000");
-//     var monthDigit = myDate.getMonth();
-//     return isNaN(monthDigit) ? 0 : (monthDigit + 1);
-// }
-
-// function convertNumberToMonth(monthNumber) {
-//     var month=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-//     return month[monthNumber - 1];
-// }
-
-
 (function () {
     'use strict';
 
@@ -19,7 +7,6 @@
 
         HelpCtrl.$inject = ['$scope','authentication','$log','$http'];
         function HelpCtrl($scope, authentication ,$log , $http) {
-
 
             $scope.convertNumberToMonth = function (monthNumber){
                 var month=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
@@ -33,16 +20,11 @@
                 return isNaN(monthDigit) ? 0 : (monthDigit);
             };
 
-            $scope.getDaysinMonth = function (month){
-                var year = new Date().getYear();
-                return new Date(year, month, 0).getDate();
-            };
-
-            // $scope.currentMonth = function () {
-            //     var date = new Date().getMonth();
-            //     return $scope.convertNumberToMonth(date);
+            // Not used
+            // $scope.getDaysinMonth = function (month){
+            //     var year = new Date().getFullYear();
+            //     return new Date(year, month, 0).getDate();
             // };
-
 
             //GET CURRENT MONTH NUMBER FOR DEFAULT CHART MONTH
             $scope.currentMonth = new Date().getMonth();
@@ -71,103 +53,52 @@
                 selectedMonth: {id: $scope.currentMonth} //This sets the default value of the select in the ui
             };
 
-            // $scope.data = {
-            //     availableMonth: [
-            //       {name: 'Jan'},
-            //       {name: 'Feb'},
-            //       {name: 'Mar'},
-            //       {name: 'Apr'},
-            //       {name: 'May'},
-            //       {name: 'Jun'},
-            //       {name: 'Jul'},
-            //       {name: 'Aug'},
-            //       {name: 'Sep'},
-            //       {name: 'Oct'},
-            //       {name: 'Nov'},
-            //       {name: 'Dec'}
-            //     ],
-            //     selectedMonth: {name: $scope.currentMonth()} //This sets the default value of the select in the ui
-            // };
+            $scope.storedData = {
+                Jan: [],
+                Feb: [],
+                Mar: [],
+                Apr: [],
+                May: [],
+                Jun: [],
+                Jul: [],
+                Aug: [],
+                Sep: [],
+                Oct: [],
+                Nov: [],
+                Dec: []
+            };
+            $scope.yCounter = [4,4,4,4,4,4,4,4,4,4,4,4];
 
-            $scope.dataCounter = 4;
-            $scope.dataArr = [];
-            $scope.dayValue = [];
-
-            $scope.getDataHC = function(result, selectedMonthName) {
-                $scope.dataArr = [];
-                var dataCounter = 4;
-
-                for (var i = 0; i < result.health.totalStepsForEachDayOfYear.length -1; i++) {
+            $scope.getDataHC = function(result) {
+                for (var i = 0; i < result.health.totalStepsForEachDayOfYear.length -1 && i < 365; i++) {
                     var test = result.health.totalStepsForEachDayOfYear[i];
 
                     //SPLIT THE dataSplit[0] = MON NUM, YEAR AND dataSplit[1] = STEPS WALKED
                     var dataSplit = test.split("-");
 
-                    //GET THE MONTH NAME FROM THE DATE
-                    var dataMonth = dataSplit[0].slice(0,3);
+                    //GET THE MONTH/DAY NAME FROM THE DATE
+                    var currentDayHolder = dataSplit[0].split(",");
+
+                    //
+                    var dataMonth = currentDayHolder[0].slice(0,3);
+                    var dataMonthDay = Number(currentDayHolder[0].slice(4,currentDayHolder[0].length));
+                    var dayYear = Number(currentDayHolder[1]);
 
                     //STORE THE STEPS
-                    var dataStep = dataSplit[1];
+                    var dataStep = Number(dataSplit[1]);
 
                     // GET THE X FOR CHART, GET THE Sunday to Monday OF DATE IN (0-6)
                     var dataDay = (new Date(dataSplit[0])).getDay();
 
-                    //SPLIT THE DATE currentDayHolder[0] = MON NUM AND currentDayHolder[1] = YEAR
-                    var currentDayHolder = dataSplit[0].split(",");
+                    var dataMonthNumber = $scope.convertMonthNameToNumber(dataMonth);
+                    $scope.storedData[dataMonth].push([dataDay,$scope.yCounter[dataMonthNumber],dataStep,dataMonthDay,dayYear]);
 
-                    // ///////////
-                    //
-
-                    //     dataSplit[0] = Number(dataDay);
-                    //  //    console.log(dataSplit[0]);
-                    //     //Y Month Number
-                    //     dataSplit[1] = $scope.convertMonthNameToNumber(dataMonth);
-                    //     // Z Daily Steps
-                    //     dataSplit[2] = Number(dataStep);
-                    //  //    console.log(dataSplit[2]);
-                    //     $scope.dataArr.push(dataSplit);
-
-                        // ////////////////////////////////////////////////
-
-                    if (dataMonth === selectedMonthName && dataDay !== undefined){
-
-                        //REMOVE THE FIRST 4 LETTERS IN currentDayHolder = (MON Number)
-                        //STORE DAY NUMBER IN MONTH DAY YEAR
-                        $scope.dayValue.push(currentDayHolder[0].slice(4));
-
-                        //MAKE dataSplit[0] Sunday to Monday OF DATE IN (0-6)
-                        dataSplit[0] = dataDay;
-                     //    console.log(dataSplit[0]);
-                        //Y
-                     //    dataSplit[1] = $scope.convertMonthNameToNumber(dataMonth);
-                     //    console.log(dataSplit[1]);
-
-                        //Y DEFAULT DATACOUNTER IS 4 SO IT STARTS AT THE TOP ROW
-                        dataSplit[1] = dataCounter;
-
-                        //ONCE THE ONCE ROW REACHES 7TH CELL GO DOWN A ROW
-                        if (dataSplit[0] === 6){
-                            dataCounter -= 1;
-                        }
-
-                        //STORE THE STEPS/Value OF USER
-                        dataSplit[2] = Number(dataStep);
-                     //    console.log(dataSplit[2]);
-
-                        //PUSH dataSplit INTO $scope.dataArr [0] = 0-6 ROW, [1] = COLUMN, [2] = STEPS/VALUE
-                        $scope.dataArr.push(dataSplit);
+                    if (dataDay === 6) {
+                        $scope.yCounter[dataMonthNumber] -= 1;
                     }
-                }
-                return $scope.dataArr;
-            };
 
-            // NOT USED
-            // $scope.getDataLabel = function (x,y) {
-            //     for (var i = 0; i < $scope.dataArr.length; i++) {
-            //         if (x === $scope.dataArr[i][0] && y === $scope.dataArr[i][1])
-            //             return $scope.dataArr[i][3];
-            //     }
-            // };
+                }
+            };
 
             $scope.highchartsNG = {
                 options: {
@@ -185,7 +116,7 @@
                                     color: '#000000',
                                     useHTML: true,
                                     formatter: function () {
-                                        return "<center>" + $scope.dayValue.shift() + "</center>" + this.point.value;
+                                        return "<center>" + $scope.highchartsNG.series[0].data[this.series.data.indexOf( this.point )][3] + "</center>" + this.point.value;
                                     }
                                 }
                             }
@@ -206,7 +137,7 @@
                            colorAxis: {
                                min: 0,
                                minColor: '#FFFFFF',
-                               maxColor: Highcharts.getOptions().colors[0]
+                               maxColor: Highcharts.getOptions().colors[7]
                            }
                        },
                        tooltip: {
@@ -242,34 +173,22 @@
             $scope.resultData = [];
 
             $scope.update = function () {
-                // var selectedMonthName = $scope.data.selectedMonth.name;
                 var selectedMonthNumber = $scope.data.selectedMonth.id;
                 var selectedMonthName = $scope.convertNumberToMonth(selectedMonthNumber);
 
-                var dataArr = $scope.getDataHC($scope.resultData, selectedMonthName);
-
-                $scope.highchartsNG.series[0].data = dataArr;
+                $scope.highchartsNG.series[0].data = $scope.storedData[selectedMonthName];
             };
 
             $http.get('/profile')
                 .success(function (result){
-                    Highcharts.setOptions({
-                      lang: {
-                        numericSymbols: null, //otherwise by default ['k', 'M', 'G', 'T', 'P', 'E']
-                        thousandsSep: ','
-                        },
-                    });
                     $scope.resultData = result;
-                    // var selectedMonthName = $scope.data.selectedMonth.name;
+
+                    $scope.getDataHC($scope.resultData);
+
                     var selectedMonthNumber = $scope.data.selectedMonth.id;
                     var selectedMonthName = $scope.convertNumberToMonth(selectedMonthNumber);
 
-
-                    var dataArr = $scope.getDataHC(result,selectedMonthName);
-
-                    $scope.highchartsNG.series[0].data = dataArr;
+                    $scope.highchartsNG.series[0].data = $scope.storedData[selectedMonthName];
                 });
-
-
         }
 }) ();
