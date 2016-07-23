b841c621d6b489c93a157fabd88e5704fad25e34
alan <alancao101@gmail.com>
2016-07-23 16:00:47 -0400

    updated service and controller

--- client/services/charts.service.js
+++ client/services/charts.service.js
@@ -11,7 +11,6 @@
         var getHealthData = function () {
             return $http.get('/profile')
                 .success(function (response) {
-                    console.log('get profile');
                     return response;
                 });
         };
@@ -21,7 +20,7 @@
         };
 
         var convertNumberToMonth = function (monthNumber) {
-            var month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+            let month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
             return month[monthNumber];
         };
 
@@ -46,15 +45,15 @@
 
           //CREATE CALENDAR
           for (let i = 1; i < 13; i++) {
-              var counter = 5;
+              let counter = 5;
 
-              var dayNumbers = getDaysinMonth(i, year);
+              let dayNumbers = getDaysinMonth(i, year);
 
               for (let j = 1; j < dayNumbers + 1; j++) {
-                  var day = i + " " + j + ", " + year;
-                  var dataDay = (new Date(day).getDay());
+                  let day = i + " " + j + ", " + year;
+                  let dataDay = (new Date(day).getDay());
 
-                  var monthName = convertNumberToMonth(i - 1);
+                  let monthName = convertNumberToMonth(i - 1);
                   storedData[monthName].push([dataDay, counter, 0, j, year]);
                   if (dataDay === 6)
                       counter -= 1;
@@ -63,10 +62,10 @@
 
           //UPDATE CALENDAR VALUE STEPS
           for (let i = 0; i < data.health.totalStepsForEachDayOfYear.length; i++) {
-              var dataSplit = data.health.totalStepsForEachDayOfYear[i].split("-");
-              var currentDayHolder = dataSplit[0].split(",");
+              let dataSplit = data.health.totalStepsForEachDayOfYear[i].split("-");
+              let currentDayHolder = dataSplit[0].split(",");
               //currentHolder[0] = Month Name currentHolder[1] = Month Day Number
-              var currentHolder = currentDayHolder[0].split(" ");
+              let currentHolder = currentDayHolder[0].split(" ");
 
               if (storedData[currentHolder[0]][Number(currentHolder[1]) - 1] !== undefined && year === Number(currentDayHolder[1]))
                   storedData[currentHolder[0]][Number(currentHolder[1]) - 1].splice(2, 1, Number(dataSplit[1]));
