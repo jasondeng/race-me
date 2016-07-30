bca0f83b7f7cff3b758a0e73e21b43435a10b87b
Jason Deng <jdeng1234@gmail.com>
2016-07-29 23:54:35 -0400

    added activity service and added icons for challenger winner and opponent

--- client/components/activity/activity.controller.js
+++ client/components/activity/activity.controller.js
@@ -5,82 +5,99 @@
         .module('app')
         .controller('ActivityLogCtrl', ActivityLogCtrl);
 
-    ActivityLogCtrl.$inject = ['$scope', 'authentication'];
-    function ActivityLogCtrl($scope, authentication) {
+    ActivityLogCtrl.$inject = ['$scope', 'authentication', 'mailbox'];
+    function ActivityLogCtrl($scope, authentication, mailbox) {
         var vm = this;
 
         $scope.currentUser = authentication.currentUser();
-        
-        $scope.races = [{
-            challenger: {
-                username: "Alan",
-                start: "7/26/2016",
-                end: "7/27/2016",
-                speed: "12",
-                duration: "12",
-                route: {
-                    origin: {
-                        lat: "40",
-                        lng: "51"
-                    },
-                    wayPoints: [[40, 41], [41, 42], [43, 44]],
-                    created: "7/12/2016"
-                }
-            },
-            opponent: {
-                username: "Satbir",
-                start: "7/23/2016",
-                end: "7/24/2016",
-                speed: "13",
-                duration: "14",
-                route: {
-                    origin: {
-                        lat: "21",
-                        lng: "21"
-                    },
-                    wayPoints: [[40, 41], [41, 42], [43, 44]],
-                    created: "7/14/2016"
-                }
-            },
-            status: "Open",
-            winner: "Satbir",
-            created: "7/16/2016"
-        },
-            {
-                challenger: {
-                    username: "Jason",
-                    start: "7/26/2016",
-                    end: "7/27/2016",
-                    speed: "12",
-                    duration: "12",
-                    route: {
-                        origin: {
-                            lat: "40",
-                            lng: "51"
-                        },
-                        wayPoints: [[40, 41], [41, 42], [43, 44]],
-                        created: "7/12/2016"
-                    }
-                },
-                opponent: {
-                    username: "Cheng",
-                    start: "7/23/2016",
-                    end: "7/24/2016",
-                    speed: "13",
-                    duration: "14",
-                    route: {
-                        origin: {
-                            lat: "21",
-                            lng: "21"
-                        },
-                        wayPoints: [[40, 41], [41, 42], [43, 44]],
-                        created: "7/14/2016"
-                    }
-                },
-                    status: "Open",
-                    winner: "Jason",
-                    created: "7/16/2016"
-            }]
+
+        vm.isLoggedIn = authentication.isLoggedIn();
+
+        vm.hasHealthData = authentication.hasHealthData();
+
+        /*
+         $scope.races = [{
+         challenger: {
+         username: "Alan",
+         start: "7/26/2016",
+         end: "7/27/2016",
+         speed: "12",
+         duration: "12",
+         route: {
+         origin: {
+         lat: "40",
+         lng: "51"
+         },
+         wayPoints: [[40, 41], [41, 42], [43, 44]],
+         created: "7/12/2016"
+         }
+         },
+         opponent: {
+         username: "Satbir",
+         start: "7/23/2016",
+         end: "7/24/2016",
+         speed: "13",
+         duration: "14",
+         route: {
+         origin: {
+         lat: "21",
+         lng: "21"
+         },
+         wayPoints: [[40, 41], [41, 42], [43, 44]],
+         created: "7/14/2016"
+         }
+         },
+         status: "Open",
+         winner: "Satbir",
+         created: "7/16/2016"
+         },
+         {
+         challenger: {
+         username: "Jason",
+         start: "7/26/2016",
+         end: "7/27/2016",
+         speed: "12",
+         duration: "12",
+         route: {
+         origin: {
+         lat: "40",
+         lng: "51"
+         },
+         wayPoints: [[40, 41], [41, 42], [43, 44]],
+         created: "7/12/2016"
+         }
+         },
+         opponent: {
+         username: "Cheng",
+         start: "7/23/2016",
+         end: "7/24/2016",
+         speed: "13",
+         duration: "14",
+         route: {
+         origin: {
+         lat: "21",
+         lng: "21"
+         },
+         wayPoints: [[40, 41], [41, 42], [43, 44]],
+         created: "7/14/2016"
+         }
+         },
+         status: "Open",
+         winner: "Jason",
+         created: "7/16/2016"
+         }]*/
+
+        vm.loadedData = false;
+        if (vm.isLoggedIn && vm.hasHealthData) {
+            $scope.loading = true;
+            mailbox.getActivityData()
+                .then(function (response) {
+                    console.log(response.data);
+                    vm.loadedData = true;
+                    $scope.loading = false;
+                    $scope.races = response.data;
+                    return response.data;
+                })
+        }
     }
-})
-();
+})();
